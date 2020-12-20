c21e312642e492638173caa9155bcf3652953f5f
keemtj <keemgreat@gmail.com>
2020-12-21 01:14:27 +0900

    :construction: Add time render function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const API_KEY = "bbcad54aeb4d627c3798f0773d883830";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 let data = [];
-let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let month = [
+const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const month = [
   "Jan",
   "Feb",
   "Mar",
@@ -21,6 +21,7 @@ let month = [
   "Nov",
   "Dec",
 ];
+
 const getTime = () => {
   const time = new Date();
   const hours = time.getHours();
@@ -28,10 +29,17 @@ const getTime = () => {
   const day = week[time.getDay()];
   const date = time.getDate();
   const moon = month[time.getMonth()];
-  return { hours, mins, day, date, moon };
+  const sec = time.getSeconds();
+
+  const clock = document.querySelector(".clock");
+  clock.innerHTML = `${hours}:${
+    mins < 10 ? "0" + mins : mins
+  } ${day} ${date} ${moon}`;
+  return { hours, mins, day, date, moon, sec };
 };
 
 const render = (city) => {
+  console.log("22222");
   const { temp } = data.main;
   const { hours, mins, day, date, moon } = getTime();
   weatherToday.innerHTML = `
@@ -41,7 +49,7 @@ const render = (city) => {
     <div class="location">
       <div class="location-name">${city}</div>
       <div class="clock">
-        ${hours}:${mins < 10 ? "0" + mins : mins} ${day} ${date} ${moon}
+      ${hours}:${mins < 10 ? "0" + mins : mins} ${day} ${date} ${moon}
       </div>
     </div>
     <div class="state">
@@ -49,10 +57,10 @@ const render = (city) => {
       <div class="weather-state">Sunny</div>
     </div>
   `;
-  getTime();
 };
 
 const getWeatherByCityName = async (city = "seoul") => {
+  console.log("11111");
   const response = await fetch(
     `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
@@ -70,5 +78,10 @@ const getWeatherByCityName = async (city = "seoul") => {
 //   return data;
 // };
 
-// Event
-window.addEventListener("load", () => getWeatherByCityName());
+const init = () => {
+  console.log("00000");
+  getWeatherByCityName();
+  setInterval(getTime, 1000);
+};
+
+init();
