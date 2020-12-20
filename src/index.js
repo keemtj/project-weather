bc9f68ce79e49d5b4d0112d87e7602c4d3336bb6
keemtj <keemgreat@gmail.com>
2020-12-21 05:01:32 +0900

    :lipstick: Add weather icon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const API_KEY = "bbcad54aeb4d627c3798f0773d883830";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 let data = [];
+const weatherIcon = [
+  "fas fa-cloud",
+  "fas fa-smog",
+  "fas fa-wind",
+  "fas fa-sun",
+  "fas fa-cloud-sun",
+  "fas fa-cloud-sun-rain",
+  "fas fa-cloud-rain",
+  "fas fa-cloud-showers-heavy",
+  "fas fa-snowflake",
+];
 const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const month = [
   "Jan",
@@ -41,20 +52,23 @@ const getTime = () => {
 const render = (city) => {
   console.log("22222");
   const { temp } = data.main;
+  const [{ main }] = data.weather;
   const { hours, mins, day, date, moon } = getTime();
   weatherToday.innerHTML = `
     <div class="temperature">
-      <span>${Math.round(temp)}</span>°
+      ${Math.round(temp)}°
     </div>
-    <div class="location">
+    <div>
       <div class="location-name">${city}</div>
       <div class="clock">
       ${hours}:${mins < 10 ? "0" + mins : mins} ${day} ${date} ${moon}
       </div>
     </div>
-    <div class="state">
-      <i class="icon-cloud"><i>
-      <div class="weather-state">Sunny</div>
+    <div class="weather">
+      <i class="${
+        weatherIcon[Math.floor(Math.random() * 10) % weatherIcon.length]
+      } icon-weather"></i>
+      <div class="weather-state">${main}</div>
     </div>
   `;
 };
@@ -78,7 +92,7 @@ const getWeatherByCityName = async (city = "seoul") => {
 //   return data;
 // };
 
-const init = () => {
+const init = async () => {
   console.log("00000");
   getWeatherByCityName();
   setInterval(getTime, 1000);
